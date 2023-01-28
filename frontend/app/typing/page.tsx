5ea9bc1d0bd33e105492b4b2a58656255706b4b8
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-01-29 06:20:16 +0900

    frontend: app/typing - get all problems fetch

--- frontend/app/typing/page.tsx
+++ frontend/app/typing/page.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Problem } from "../../typings";
 
-function Typing() {
+async function getProblems() {
+  const url = "http://127.0.0.1:8000/problems";
+  const res = await fetch(url);
+  return res.json();
+}
+
+async function Typing() {
+  const problems: Problem[] = await getProblems();
+
   return (
     <div className="text-2xl p-4">
       <h2 className="text-xl">Problems</h2>
+
+      <ul>
+        {problems.map((problem) => (
+          <li key={problem.id} className="text-lg">
+            {problem.id}. {problem.sentence.substring(0, 50)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
